9ac0bf87f19824ca592d6e2d6ecda06bf245c774
ITXlebyshek <62103545+ITXlebyshek@users.noreply.github.com>
2021-08-02 18:32:28 +0500

    Бета версия

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+AOS.init({
+	once: true,
+});
+
 var swiper = new Swiper(".intro-slider", {
-    navigation: {
-      nextEl: "#intro .swiper-button-next",
-      prevEl: "#intro .swiper-button-prev",
-    },
-  });
+	navigation: {
+		nextEl: "#intro .swiper-button-next",
+		prevEl: "#intro .swiper-button-prev",
+	},
+});
+
+$(document).ready(function () {
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+		$(this).toggleClass('open');
+	});
+});
