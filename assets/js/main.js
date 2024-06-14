efd75197f2845b8e345cc9aca2999106130f6554
ITXlebyshek <62103545+ITXlebyshek@users.noreply.github.com>
2024-06-14 13:18:47 +0500

    Сайт сталь для демо показа

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+window.addEventListener('click', e => {
+	const target = e.target
+
+	// Скрытие открытие шапки
+
+	if (target.closest('.header__btn-mob, .header__nav-close')) {
+		$('.header__nav').toggleClass('show');
+		$('.hamburger-lines').toggleClass('close');
+	};
+
+	// Выбор языка
+	if (target.closest('.switch-lang__curr, .switch-lang__item') || (!target.closest('.switch-lang__list.show') && $(document).find('.switch-lang__list').hasClass('show'))) {
+		$('.switch-lang__list').toggleClass('show');
+	}
+})
+
+
+$(document).ready(function () {
+
+
+	if ($("#lightgallery").length > 0) {
+		$("#lightgallery").lightGallery();
+		$("#lightgallery1").lightGallery();
+	}
+
+	$(document.body).click(function () {
+		$(".lg-image").each(function () {
+			if ($(this).css("opacity") === "1") { $('.admin').show(); }
+			else {
+				$('.admin').hide();
+			}
+
+		})
+	});
+});
